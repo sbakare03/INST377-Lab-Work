33fe8fc867330ff5304c8417157d739417d1f42e
sbakare03 <98419213+sbakare03@users.noreply.github.com>
2022-02-28 17:51:36 -0500

    updated script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,40 +1,39 @@
-const slidePosition = 0;
+let slidePosition = 0;
 const slides = document.querySelectorAll('carousel_item');
 const totalSlides = slides.length;
 
-document.
-querySelector('carousel_button--next')
-.addEventListener("click", function() => {
-    moveToNextSlide();
-   });
-
-  document.
-  querySelector('carousel_button--prev')
-  .addEventListener("click", function() => {
-      moveToPrevSlide();
-    });
-
 function updateSlidePosition() {
-    for (let slide of slides) {
-        console.log(slide);
-    }
+  for (const slide of slides) {
+    console.log(slide);
+  }
 }
 
-
 function moveToNextSlide() {
-    updateSlidePosition();
-    if(slidePosition = totalSlides) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
+  updateSlidePosition();
+  if (slidePosition === totalSlides) {
+    slidePosition = 0;
+  } else {
+    slidePosition+=1;
+  }
 }
 
 function moveToPrevSlide() {
-    updateSlidePosition();
-    if(slidePosition = 0) {
-        slidePosition = 0;
-    } else {
-        slidePosition--;
-    }
+  updateSlidePosition();
+  if (slidePosition === 0) {
+    slidePosition = 0;
+  } else {
+    slidePosition--;
+  }
 }
+
+document
+  . querySelector('.carousel_button--next')
+  .addEventListener('click', () => {
+    moveToNextSlide();
+  });
+
+document
+  .querySelector('.carousel_button--prev')
+  .addEventListener('click', () => {
+    moveToPrevSlide();
+  });
