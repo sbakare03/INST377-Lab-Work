b97f8fb40ff0c05dd00364b002808e221dadf2ba
sbakare03 <98419213+sbakare03@users.noreply.github.com>
2022-02-28 18:27:45 -0500

    updated script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,20 +1,23 @@
 let slidePosition = 0;
-const slides = document.querySelectorAll('carousel__item');
+const slides = document.querySelectorAll('.carousel__item');
 const totalSlides = slides.length;
 
 function updateSlidePosition() {
   for (const slide of slides) {
     console.log(slide);
+    slide.classList.add('carousel__item--hidden');
+    slide.classList.remove('carousel__item--visible');
   }
+  slides[slidePosition].classList.add('carousel__item--visible');
 }
 
 function moveToNextSlide() {
-  updateSlidePosition();
   if (slidePosition === totalSlides) {
     slidePosition = 0;
   } else {
-    slidePosition+=1;
+    slidePosition += 1;
   }
+  updateSlidePosition();
 }
 
 function moveToPrevSlide() {
@@ -29,13 +32,13 @@ function moveToPrevSlide() {
 document
   .querySelector('#carousel__button--next')
   .addEventListener('click', () => {
-      console.log("clicked next")
+    console.log('clicked next');
     moveToNextSlide();
   });
 
 document
   .querySelector('#carousel__button--prev')
   .addEventListener('click', () => {
-    console.log("clicked prev")
+    console.log('clicked prev');
     moveToPrevSlide();
   });
